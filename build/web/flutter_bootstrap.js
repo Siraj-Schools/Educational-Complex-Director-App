b14a1c2947cdb4ebfffca83bcd16e92683cc59b9
suhaibhafez <suhaibhafez78@gmail.com>
2026-02-28 08:48:43 +0300

    second build

--- build/web/flutter_bootstrap.js
+++ build/web/flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"587c18f873b8ab57330422bce09047420d9c7f
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3285226304"
+    serviceWorkerVersion: "4097238230"
   }
 });
